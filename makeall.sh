034926e95435fb8daa8333370f9837350f72280d
Jordi Sanfeliu <jordi@fibranet.cat>
2025-10-16 18:04:28 +0200

    updated build of 'file-5.46'

--- makeall.sh
+++ makeall.sh
@@ -667,7 +667,7 @@ EOF
 			_unpack $prg-$ver z
 			pushd $prg-$ver || exit 1
 				_patch $prg-$ver
-				./configure --prefix=/usr --disable-shared || exit 1
+				./configure --prefix=/usr || exit 1
 				_make
 			popd
 			_pack $prg-$ver
